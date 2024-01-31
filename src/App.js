73ec9d4e806a7a6400e436f4c2fa42ca3fdde26f
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2024-01-31 23:13:27 +0200

    add emailjs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
@@ -9,9 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} >
           <Route index element={<Home />} />
-        </Route>
-        
-        {/* <Route path="dashboard" element={<Dashboard />} /> */}  
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>  
       </Routes>
     </>
   );
